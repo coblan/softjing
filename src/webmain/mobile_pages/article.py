c2d2ee0f7b831d68f9021271447a216653a2aae5
he_yulin <he_yulin@163.com>
2020-04-03 13:34:48 +0800

    debug

--- src/webmain/mobile_pages/article.py
+++ src/webmain/mobile_pages/article.py
@@ -1,17 +1,47 @@
 from .home import Home
 from hello.engin_menu import mb_page
+from helpers.mobile.shortcut import ModelTableMobile
+from helpers.director.shortcut import director
+from helpers.func.html import truncatehtml
+from webmain.models import Article
 
 class ArticlePage(Home):
     def get_context(self): 
         return {
-            'editor':'live_layout',
+            #'editor':'live_layout',
+            #'editor_ctx':{
+                #'layout_editors':[
+                    ##{'editor':'com-top-html','html':'<h2 style="text-align:center;margin:.4rem 0 .2rem 0">最新文章</h2>'},
+                    #{'editor':'com-live-list',}
+                    
+                #],
+                #'footer':self.get_footer(2)
+            #}
+            'editor':'live_list',
             'editor_ctx':{
-                'layout_editors':[
-                    {'editor':'com-top-html','html':'建站心得体会,敬请期待!'}
-                ],
-                'footer':self.get_footer(2)
-            }
+                **ArticleTable().get_head_context(),
+                 'table_editor': 'com-list-general',
+                 'row_editor':'com-top-caption',
+                 'title':'文章推荐',
+                 },
+            
+            
         }
+
+class ArticleTable(ModelTableMobile):
+    nolimit = True
+    model = Article
+    exclude = []
+    
+    def dict_row(self, inst):
+        return {
+            'image_url':inst.cover,
+            'sub_title':truncatehtml(inst.content,length=30),
+        }
+
+director.update({
+    'article':ArticleTable,
+})
 
 mb_page.update({
     'article':ArticlePage
